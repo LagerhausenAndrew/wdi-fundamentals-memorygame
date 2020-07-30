fbdad916efea741d829d3d39b8715840856352c8
Lagerhausen Andrew P <LagerhausenAndrewP@JohnDeere.com>
2020-07-29 19:56:24 -0500

    created game logic

--- memory-game/memory-game/js/main.js
+++ memory-game/memory-game/js/main.js
@@ -1,7 +1,16 @@
 console.log("Up and running!");
-let cardOne = "queen";
-let cardTwo = "queen";
-let cardThree = "king";
-let cardFour = "king";
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+//console.log("User flipped " + cardOne);
+//console.log("User flipped " + cardThree);
+let cards = ["queen", "queen", "king", "king"];
+let cardsInPlay = [];
+var cardOne = cards[0];
+cardsInPlay.push(cardOne,);
+console.log("User flipped queen");
+var cardTwo = cards[1];
+cardsInPlay.push(cardTwo);
+console.log("User flipped queen");
+if (cardsInPlay.length === 2 && cardOne === cardTwo) {
+alert("You found a match!");
+}else {
+alert("Sorry, try again");
+}
